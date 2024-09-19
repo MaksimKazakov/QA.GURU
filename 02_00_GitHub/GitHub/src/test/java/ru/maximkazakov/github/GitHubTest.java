58b5e0eda3fdbd90e04f3e2cec1dcdf9b37c5e64
Maxim Kazakov <kazakovms@a-trast.ru>
2024-09-19 19:20:59 +0500

    Третий урок - добавил новую ветку

--- 02_00_GitHub/GitHub/src/test/java/ru/maximkazakov/github/GitHubTest.java
+++ 02_00_GitHub/GitHub/src/test/java/ru/maximkazakov/github/GitHubTest.java
@@ -2,6 +2,6 @@ package ru.maximkazakov.github;
 
 public class GitHubTest {
     public static void main(String[] args) {
-        System.out.println("Hello World - main branch");
+        System.out.println("Hello World - main new-branch");
     }
 }
